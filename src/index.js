7ab056c3a67139ef903ee82fc23ca0447712b165
wellingtonlopes <ookami.seta@gmail.com>
2020-09-13 15:09:10 -0400

    first try

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import initTilt from './js/tilt';
+import initSr from './js/sr';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './style/main.scss';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+initSr();
+initTilt();
